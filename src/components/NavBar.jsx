684514ee7ef714c19d34e7bfc20f2f05b2d87824
khoale38 <kevinle38@gmail.com>
2022-08-03 16:35:18 +0700

    fix scroll

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,16 +7,16 @@ const NavBar = () => {
     const handleClick = () => setNav(!nav)
     const handleClosed = () => setNav(!nav)
     return (
-        <div className='w-screen h-[80px] z-10 bg-zinc-200 drop-shadow-lg'>
+        <div className='w-full h-[80px] z-10 bg-zinc-200 drop-shadow-lg'>
             <div className='px-2 flex justify-between items-center w-full h-full'>
                 <div className='flex  items-center'>
                     <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND </h1>
                     <ul className='hidden md:flex'>
-                        <li><Link to="home" smooth={true} offset={50} duration={500} ></Link>Home</li>
-                        <li><Link to="about" smooth={true} offset={50} duration={500} ></Link>About</li>
-                        <li><Link to="support" smooth={true} offset={50} duration={500} ></Link>Support</li>
-                        <li><Link to="platform" smooth={true} offset={50} duration={500} ></Link>Platform</li>
-                        <li><Link to="pricing" smooth={true} offset={50} duration={500} ></Link>Pricing</li>
+                        <li><Link to="home" smooth={true} offset={50} duration={500} >Home</Link></li>
+                        <li><Link to="about" smooth={true} offset={50} duration={500} >About</Link></li>
+                        <li><Link to="support" smooth={true} offset={50} duration={500} >Support</Link></li>
+                        <li><Link to="platform" smooth={true} offset={50} duration={500} >Platform</Link></li>
+                        <li><Link to="pricing" smooth={true} offset={50} duration={500} >Pricing</Link></li>
                     </ul>
                 </div>
                 <div className='hidden md:flex mr-4'>
@@ -31,11 +31,11 @@ const NavBar = () => {
                 </div>
             </div>
             <ul className={nav ? 'absolute bg-zinc-200 w-full px-8' : 'hidden'}>
-                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClosed} to="home" smooth={true} offset={50} duration={500} ></Link> Home</li>
-                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClosed} to="about" smooth={true} offset={50} duration={500} ></Link> About</li>
-                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClosed} to="support" smooth={true} offset={50} duration={500} ></Link> Support</li>
-                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClosed} to="platform" smooth={true} offset={50} duration={500} ></Link> Platform</li>
-                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClosed} to="pricing" smooth={true} offset={50} duration={500} ></Link> Pricing</li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClosed} to="home" smooth={true} offset={50} duration={500} >Home</Link> </li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClosed} to="about" smooth={true} offset={50} duration={500} >About</Link> </li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClosed} to="support" smooth={true} offset={50} duration={500} >Support</Link> </li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClosed} to="platform" smooth={true} offset={50} duration={500} >Platform</Link> </li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClosed} to="pricing" smooth={true} offset={50} duration={500} >Pricing</Link> </li>
                 <div className='flex flex-col my-4'>
                     <button className=' bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
                     <button className='px-8 py-3'>Sign Up</button>
